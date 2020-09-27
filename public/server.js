9297ba537168d641cd8afac5e89ccd04dece8b13
Nou Vang <nou.muas@gmail.com>
2020-09-27 18:34:58 -0500

    debug

---
+++ public/server.js
@@ -0,0 +1,14 @@
+const app = require("express")();
+const path = require("path")
+
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname,"../public/notes.html"))
+})
+
+app.get("*", function(req, res){
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.listen(3000, function() {
+    console.log("hosting on 3000")
+})
