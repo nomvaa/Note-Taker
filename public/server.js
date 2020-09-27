3e97784b52669ac2efb5e1e314ff72f900833367
Nou Vang <nou.muas@gmail.com>
2020-09-27 18:52:56 -0500

    debug

--- public/server.js
+++ public/server.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const path = require("path")
 
 app.get("/notes", function(req, res){
-    res.sendFile(path.join(__dirname,"../public/notes.html"))
+    res.sendFile(path.join(__dirname, "notes.html"))
 })
 
 app.get("*", function(req, res){
