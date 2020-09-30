ead30014329ca6c61e9fb2fa31f0a2559e5d7f2d
Nou Vang <nou.muas@gmail.com>
2020-09-29 23:33:47 -0500

    modified routerjs

--- public/assets/js/router.js
+++ public/assets/js/router.js
@@ -2,10 +2,10 @@ var router =require("express").Router();
 var path =require("path");
 
 router.get("/notes", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/notes.html"))
+    res.sendFile(path.join(__dirname, "../..", "notes.html"))
 })
 router.get("*", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/index.html"))
+    res.sendFile(path.join(__dirname, "../..", "index.html"))
 })
 
 module.exports = router
