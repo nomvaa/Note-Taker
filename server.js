8b575e4472c43a9de329d1b0c74a1060f2a6f14a
Nou Vang <nou.muas@gmail.com>
2020-09-30 17:53:03 -0500

    update server

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 
-// app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use('/api/notes', apiRoutes);
 app.use('/', htmlRoutes);
 
